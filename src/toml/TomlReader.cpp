6f992da0bbc243754ef4f5d5d7c7edcb9322893f
Wiktor <wiktor.markowski81@gmail.com>
2026-09-14 14:16:12 +0200

    Set currTomlValue to TomlObject in dotted table case

--- src/toml/TomlReader.cpp
+++ src/toml/TomlReader.cpp
@@ -149,6 +149,8 @@ Marco::TomlError Marco::TomlReader::HandleTables(Marco::TomlValue& rootTomlValue
 			}
 			
 			currTomlValue = &(*currTomlValue)[key];
+			*currTomlValue = TomlObject{};
+			
 			key = "";
 			
 
